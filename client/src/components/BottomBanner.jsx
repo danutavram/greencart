888306d5ee802c7ee1456c9be998d476934bc5f4
danutavram <avramnicolaedan@gmail.com>
2025-08-15 21:47:18 +0300

    BottomBanner

---
+++ client/src/components/BottomBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const BottomBanner = () => {
+  return (
+    <div className='relative mt-24'>
+        <img src={assets.bottom_banner_image} alt="banner" />
+    </div>
+  )
+}
+
+export default BottomBanner

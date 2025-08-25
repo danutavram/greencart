d51383f47cc7ca670995596db57fa6375310348f
danutavram <avramnicolaedan@gmail.com>
2025-08-25 18:02:33 +0300

    User/userController/userRoute "registering user"

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors'
+import connectDB from './configs/db.js';
+import 'dotenv/config'
+import userRouter from './routes/userRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+await connectDB()
 
 // Akkiw multiple origins
 const allowedOrigins = ['http://localhost:5173']
@@ -14,6 +19,7 @@ app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
 
 app.get('/', (req, res)=> res.send("API is Working"));
+app.use('/api/user', userRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${[port]}`)

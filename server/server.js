a8b40575806fafac1b1c0d14ef68db3a2293e31e
danutavram <avramnicolaedan@gmail.com>
2025-08-29 14:40:41 +0300

    project completed

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Akkiw multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-three-hazel.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
